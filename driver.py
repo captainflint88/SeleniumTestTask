f63ca515da60544098f5992c5c7a70d5b3015c82
pavel.blinkov <pavel.blinkov@gs-labs.ru>
2022-11-25 03:20:37 +0500

    init

--- driver.py
+++ driver.py
@@ -1,15 +1,12 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
 class BaseTest(unittest.TestCase):
 
     def setUp(self):
-        caps = DesiredCapabilities.CHROME
-
         options = webdriver.ChromeOptions()
         options.add_argument("--no-sandbox")
         options.add_argument("--headless")
@@ -18,7 +15,6 @@ class BaseTest(unittest.TestCase):
         self.driver = webdriver.Remote(
             command_executor='http://localhost:4444/wd/hub',
             options=webdriver.ChromeOptions(),
-            desired_capabilities=caps,
             keep_alive=False
         )
         self.driver.implicitly_wait(15)
